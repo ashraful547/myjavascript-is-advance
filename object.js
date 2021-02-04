0c369074c2126e543fa4764be7d8c0b485162c06
ashraful547 <alfatulk@gmail.com>
2021-02-05 02:40:50 +0600

    hello bro

--- object.js
+++ object.js
@@ -15,4 +15,6 @@ const names = students.map(s => s.name);
 const ids = students.map(s => s.id);
 const bigger = students.filter(s => s.id > 40);
 const biggerOne = students.find(s => s.id > 40);
+
 console.log(biggerOne);
+
