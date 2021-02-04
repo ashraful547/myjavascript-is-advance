31e1052246cfa7c960dd002ed19b58e69ebcc91c
ashraful547 <alfatulk@gmail.com>
2021-02-04 22:30:00 +0600

    hello bro

---
+++ trueFalse.js
@@ -0,0 +1,23 @@
+
+//falsy:
+//0
+//"";
+//false
+//undenfined
+//null
+// const age = " ";
+//NaN
+
+
+//truthy
+//"0", ' ', []
+
+let name = null;
+console.log(name);
+
+if(name){
+    console.log("condition is true");
+}
+else{ 
+    console.log(" condition is false");
+}
